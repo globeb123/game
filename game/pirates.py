9d799b5e50c2e1f9f3cedec2cafeeadd63ef235f
Rakitin Alexander <sashka.r88@gmail.com>
2024-06-11 21:29:40 +0300

    sorted

--- game/pirates.py
+++ game/pirates.py
@@ -5,8 +5,8 @@ import pygame.font
  
 pygame.init()
 
-win_width = 1200
-win_height = 900
+win_width = 1100
+win_height = 800
 bg = pygame.image.load("game/images/background.png")
 
 bg = pygame.transform.scale(bg, (win_width, win_height))
@@ -18,22 +18,25 @@ window_rect = window.get_rect()
 
 pygame.display.set_caption("PIRATES")
 exit = False
-    
+
+block = pygame.image.load("game/images/grass.png")
+
 while not exit:
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:
             exit = True
     window.blit(bg, (0, 0))
+    window.blit(block, (200, 200))
     pygame.display.update()
 
-class block(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height) -> None:
+block = pygame.transform.scale(block, (win_width, win_height))
+
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, new_image, x, y, width, height) -> None:
         super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill('red')
+        self.image = pygame.transform.scale(pygame.image.load(new_image), (width, height))
         self.rect = self.image.get_rect()
-        self.init_x = x
-        self.init_y = y
         self.rect.x = x
         self.rect.y = y
-        self.width = width
+    def show(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
