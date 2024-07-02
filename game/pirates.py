b1526af3b07d9006b4157b9ff34197588c6c8a79
Rakitin Alexander <sashka.r88@gmail.com>
2024-07-02 19:55:14 +0300

    Reworked

--- game/pirates.py
+++ game/pirates.py
@@ -1,11 +1,14 @@
-import pygame
-import pygame.font
+import pygame 
  
- 
+score1 = 0
+score2 = 0
+
+score11 = str(score1)
+score12 = str(score2)
  
 pygame.init()
 
-win_width = 1100
+win_width = 900
 win_height = 600
 bg = pygame.image.load("game/images/background.png")
 
@@ -27,12 +30,14 @@ margin = 10
 block = pygame.image.load("game/images/grass.png")
 block1 = pygame.transform.scale(block, (width, height))
 
-window.blit(bg, (0, 0))
+ship = pygame.image.load("game/images/ship.png")
+ship1 = pygame.transform.scale(ship, (width, height))
+
+font = pygame.font.Font(None, 36)
 
 mas = [[0]*10 for i in range(10)]
 
 while not exit:
-    pygame.display.update()
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:
             exit = True
@@ -43,13 +48,17 @@ while not exit:
             row = y_mouse//(margin+height)
             mas [row] [column] = 1
 
-for row in range (10):
-    for col in range(10):
-        if mas [row] [column] == 1:
-            block = pygame.image.load("game/images/grass.png")
-        else:
-            block = pygame.image.load("game/images/ship.png")
-        x = col * width + (col + 1)*margin
-        y = row * height + (row + 1)*margin
-    
-        window.blit(block1, (x, y))
+    window.blit(bg, (0, 0))
+    for row in range (10):
+        for col in range(10):
+            if mas [row][col] == 1:
+                block = ship1
+            else:
+                block = block1
+            x = col * width + (col + 1)*margin
+            y = row * height + (row + 1)*margin
+        
+            window.blit(block, (x, y))
+    window.blit(font.render('Score ' + score11 + " : " + score12, True, (0, 0, 0)), (700, 100))
+    clock.tick(60)
+    pygame.display.update()
